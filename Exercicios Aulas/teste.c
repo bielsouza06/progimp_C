28b5120896d9b113502717c30ba0e15dc78ec732
bielsouza06 <gabriel.souza@dcomp.ufs.br>
2019-10-22 15:39:13 -0300

    Testando

---
+++ Exercicios Aulas/teste.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( void )
+
+{
+	int x;
+	printf ("Insira um numero \n");
+	scanf ("%d",&x);
+	printf ("Seu numero mais 2: %d",x+2);
+	return 0;
+}
