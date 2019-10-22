c0cdf75d4163cd46095d401b8709c0959cb7aea5
bielsouza06 <gabriel.souza@dcomp.ufs.br>
2019-10-22 16:12:55 -0300

    Exercicio do Silide

---
+++ Exercicios Aulas/Atv01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+
+{
+int n1,n2,n3,n4;
+
+   printf ("Insira quatro numeros em sequencia \n");
+   scanf  ("%d %d %d %d",&n1,&n2,&n3,&n4);
+   printf ("A soma desses numeros: %d",n1+n2+n3+n4);
+   return 0;
+
+}
