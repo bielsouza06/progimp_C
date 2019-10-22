5971faf60244c56393572132015b57ab7620fc67
bielsouza06 <gabriel.souza@dcomp.ufs.br>
2019-10-22 16:30:31 -0300

    Exercicio - Slide

---
+++ Exercicios Aulas/Atv02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+
+{
+
+float nt1,nt2,nt3,media;
+
+   printf ("Insira a primeira nota \n");
+   scanf  ("%f",&nt1);
+   printf ("Insira a segunda nota \n");
+   scanf  ("%f",&nt2);
+   printf ("Insira a terceira nota \n");
+   scanf  ("%f",&nt3);
+   media = (nt1+nt2+nt3)/3;
+   printf ("%f", media);
+
+   return 0;
+}
