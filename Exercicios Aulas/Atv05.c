44f2db73b684616b1def1a929c464e0e185078bd
bielsouza06 <gabriel.souza@dcomp.ufs.br>
2019-10-24 16:16:25 -0300

    Atividade - Slide

---
+++ Exercicios Aulas/Atv05.c
@@ -0,0 +1,21 @@
+/*Programa que recebe um numero, calcula e o mostra 
+ao quadrado, cubo, raiz quadrada e raiz cubica.*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+
+{
+
+float num, quad, cubo, r2, r3;
+  scanf  ("%f",&num);
+  quad = num*num;
+  cubo = pow (num,3);
+  r2 = sqrt (num);
+  r3 = pow (num, (1.0/3.0));
+  printf ("Quadrado:%f\nCubo:%f\nRaiz2:%f\nRaiz3:%f\n",quad,cubo,r2,r3);
+
+  return 0;
+
+}
