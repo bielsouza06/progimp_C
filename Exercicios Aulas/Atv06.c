b757f6893c9721b21b95f7c72e37bb0559d60b3c
bielsouza06 <gabriel.souza@dcomp.ufs.br>
2019-11-06 17:50:28 -0300

    Ajuste

--- Exercicios Aulas/Atv06.c
+++ Exercicios Aulas/Atv06.c
@@ -12,7 +12,7 @@ int main (void)
 
   printf ("Insira as 3 notas do aluno\n");
   scanf  ("%f %f %f",&n1,&n2,&n3);
-  media = (n1+n2+n3)/3.0;
+  media = (n1*2.0+n2*3.0+n3*5.0)/10.0;
   if (media >= 8.0) {printf ("Média: %f\nConceito: A",media);}
    else if (media >= 7.0) {printf ("Média: %f\nConceito: B",media);}
    else if (media >= 6.0) {printf ("Média: %f\nConceito: C",media);}
