831abf8a582e3ac5b23d74244f2b525834d25bd3
bielsouza06 <gabriel.souza@dcomp.ufs.br>
2019-11-07 17:46:41 -0300

    Atv-Slides

--- Exercicios Aulas/Atv10.c
+++ Exercicios Aulas/Atv10.c
@@ -14,10 +14,10 @@ salario = 1000.0;
 perc = 1.5/100.0;
 novosal = salario + (perc*salario);
 
-for (i=2007.0; i <= ano;i++){
+for (i=2007.0; i <= ano; i++){
      perc=2.0*perc;
      novosal = novosal + (novosal*perc);}
 
-printf ("Salario atual: %.2f",novosal);
+printf ("Salario atual: %2f",novosal);
 return 0;
 }
